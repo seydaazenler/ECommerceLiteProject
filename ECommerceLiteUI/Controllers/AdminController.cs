1685782e1f3994fdedabb2358c12f4efd115a311
Şeyda Zenler <seydaazenler@gmail.com>
2022-01-20 15:00:42 +0300

    AdminController Dashboard ekranı eklendi

---
+++ ECommerceLiteUI/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ECommerceLiteUI.Controllers
+{
+    public class AdminController : BaseController
+    {
+        // GET: Admin
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
---
+++ ECommerceLiteUI/Controllers/PartialsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ECommerceLiteUI.Controllers
+{
+    public class PartialsController : BaseController
+    {
+        public PartialViewResult AdminSideBarResult()
+        {
+            //TODO: name surname alınacak
+            TempData["NameSurname"] = "";
+            return PartialView("_PartialAdminSideBar");
+        }
+       public PartialViewResult AdminSideBarMenuResult()
+        {
+
+            return PartialView("_PartialAdminSideBarMenu");
+        }
+        
+    }
+}
